3a8679628cc89948fa708b93751f44b1885317f3
Jordancoyle <engineerofsoftware@gmail.com>
2015-09-22 16:39:10 +0100

    eurrur

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,12 @@
 
 int main(int argc, char * arg[])
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "error SDL_Init" << SDL_GetError() << std::endl;
+
+		return -1;
+	}
+
     return 0;
 }
