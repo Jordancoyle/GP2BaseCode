e33ddf1ac459db3d92b8590577768f2e8a299bbd
Jordancoyle <engineerofsoftware@gmail.com>
2015-09-29 16:21:48 +0100

    Started

--- include/Graphics.h
+++ include/Graphics.h
@@ -1,7 +1,15 @@
 #ifndef _GRAPHICS_H
 #define _GRAPHICS_H
 
-void initOpenGL();
+void initOpenGL()
+{
+	GLenum err = glewInit();
+
+	if (GLEW_OK != err)
+	{
+		std::cout << "error:" << glewGetErrorString(err) << std::endl;
+	}
+}
 
 void setViewport( int width, int height );
 
