a0a7ee26cdc9afedc4e0dbc21af464376a1028c7
Jordancoyle <engineerofsoftware@gmail.com>
2015-10-06 14:50:55 +0100

    Prep

---
+++ include/Shader.h
@@ -0,0 +1,6 @@
+#ifndef _Shader_h
+#define _Shader_h
+
+#include "common.h"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -63,38 +63,7 @@ void render()
     //clear the colour and depth buffer
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexPointer(3, GL_FLOAT, sizeof(Vertex), NULL);
-	glColorPointer(4, GL_FLOAT, sizeof(Vertex), (void**)(3 * sizeof(float)));
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_COLOR_ARRAY);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glTranslatef(0.0f, 0.0f, -6.0f);
 	glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(GLuint), GL_UNSIGNED_INT, 0);
-
-    //Swith to ModelView
-    glMatrixMode( GL_MODELVIEW );
-    //Reset using the Indentity Matrix
-    glLoadIdentity( );
-
-	gluLookAt(0.0, 0.0, 6.0, 0.0, 0.0, -1.0f, 0.0, 1.0, 0.0);
-    //Do translation, push the next bit of drawing 'back' 5 units
-    //on z-zaxis
-    //Everyting after this will be drawn at -5.0f on z-axis
-    //until reset by glLoadIdentity!
-    glTranslatef( -5.0f, 0.0f, -5.0f );
-
-    //Begin drawing triangles
-    glBegin( GL_TRIANGLES );
-      glColor3f(1.0f, 0.0f, 0.0f); //Colour of the vertices
-      glVertex3f(  0.0f,  1.0f, 0.0f ); // Top
-      glVertex3f( -1.0f, -1.0f, 0.0f ); // Bottom Left
-      glVertex3f(  1.0f, -1.0f, 0.0f ); // Bottom Right
-    glEnd( );
 
 }
 
@@ -127,6 +96,10 @@ int main(int argc, char * arg[])
 
         return -1;
     }
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
     //Create a window
     SDL_Window * window = SDL_CreateWindow(
