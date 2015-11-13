c788d16094809d9626c8e94c2e5648bb9e35c9af
Jordancoyle <engineerofsoftware@gmail.com>
2015-11-13 13:02:00 +0000

    GameObject

--- include/GameObject.h
+++ include/GameObject.h
@@ -1,18 +1,45 @@
 #ifndef _GAMEOBJECT_H
 #define _GAMEOBJECT_H
 
-#include "Common.h"
+#include "common.h"
+#include "Vertices.h"
 
 class GameObject
 {
 public:
 	GameObject();
 	~GameObject();
+	
+	void update();
+	void createBuffer(Vertex *pVerts, int numVerts, int* pIndices, int numIndices);
+	void loadShader(const string& vsFilename, const string& fsFilename);
 
-	void addChild(shared_ptr<GameObject> child);
+	GLuint getShaderProgram()
+	{
+		return m_shaderProgram;
+	}
 private:
-	vector<shared_ptr<GameObject> > m_ChildObjects;
-};
+	GLuint m_VBO;
+	GLuint m_EBO;
+	GLuint m_VAO;
+	GLuint m_shaderProgram;
 
+	int m_NumOfVertices;
+	int m_NumOfIndices;
+
+	mat4 m_ModelMatrix;
+
+	vec3 m_Position;
+	vec3 m_Rotation;
+	vec3 m_Scale;
+
+	vec4 m_AmbientMaterial;
+	vec4 m_DiffuseMaterial;
+	vec4 m_SpecularMaterial;
+	float m_SpecularPower;
+
+protected:
+
+};
 
 #endif
